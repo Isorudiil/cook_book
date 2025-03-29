4957635cc6cf66546b68c96b7d409bb9d37af416
Elijah Herald <elijah.herald@gmail.com>
2025-03-28 21:55:38 -0400

    fixed typos

--- app/models.py
+++ app/models.py
@@ -17,7 +17,7 @@ class User(UserMixin, db.Model):
         return '<User {}>'.format(self.username)
     
     def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
+        self.password_hash = generate_password_hash(password,'pbkdf2')
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
